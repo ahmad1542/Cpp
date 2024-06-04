a775c7bac9403ca017d772babd09ae9a61b842f2
ahmad1542 <ahmad.husien4@gmail.com>
2024-06-04 22:32:35 +0300

    Problem 47 solved

--- Abu-Hadhoud Course(4)/Abu-Hadhoud Course (4)/Abu-Hadhoud Course (4).cpp
+++ Abu-Hadhoud Course(4)/Abu-Hadhoud Course (4)/Abu-Hadhoud Course (4).cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 
-int ReadPositiveNumber(string message) {
-	int num;
+double readNumber(string message) {
+	double num;
 
 	cout << message;
 	cin >> num;
@@ -10,52 +10,24 @@ int ReadPositiveNumber(string message) {
 	return num;
 }
 
-int RandomNumber(int From, int To) { 	
-	int randNum = rand() % (To - From + 1) + From;
-	return randNum;
-} 
-
-void AddArrayElement(int Number, int arr[100], int& arrLength) {
-	arrLength++;
-	arr[arrLength - 1] = Number;
-} 
-
-
-void Fill_Array(int arr[100], int& arrLength) {
-	int randNum;
-	for (int i = 0; i < arrLength; i++) {
-		randNum = RandomNumber(1, 100);
-		arr[i] = randNum;
-	}
+double getFractionPart(double number) {
+	return number - (int)number;
 }
 
-void CopyArrayElements(int primaryArr[100], int secondaryArr[100], int arrLength, int& secArrLength) {
-	for (int i = 0; i < arrLength; i++) {
-		AddArrayElement(primaryArr[i], secondaryArr, secArrLength);
-	}
-}
-
-void Print_Array(int arr[100], int arrLength) {
-
-	for (int i = 0; i < arrLength; i++)
-		cout << arr[i] << " ";
-
-	cout << "\n";
+int roundFunction(double number) {
+	double fractionalPart = getFractionPart(number);
+	if (fractionalPart >= 0.5)
+		return ((int)number + 1);
+	else
+		return (int)number;
 }
 
 int main() {
-	srand((unsigned)time(NULL));
 
-	int primaryArr[100], secArr[100];
-	int primaryArrLength = ReadPositiveNumber("Please enter array size: ");
-	int secArrLength = 0;
-	Fill_Array(primaryArr, primaryArrLength);
-	cout << "Array 1 elements: " << endl;
-	Print_Array(primaryArr, primaryArrLength);
-
-	CopyArrayElements(primaryArr, secArr, primaryArrLength, secArrLength);
-	cout << "Array 2 elements: " << endl;
-	Print_Array(secArr, secArrLength);
+	double number = readNumber("Enter number: ");
+	
+	cout << "My round function: " << roundFunction(number) << endl;
+	cout << "C++ round function: " << round(number) << endl;
 
 	return 0;
 }
